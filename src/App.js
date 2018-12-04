cf4c07a9ace7657328dbf5e93f9792f20c5a77ca
Marcus Davanço <md.daves@gmail.com>
2018-12-03 23:50:51 -0200

    Simple react conversion

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Site from './Site'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Site />
+      </div>
+    );
+  }
+}
+
+export default App;
